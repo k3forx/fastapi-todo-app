328f4913708dd0b1c93ad498a5e777ada800fac4
Kanata Miyahana <afiri.bupa@gmail.com>
2021-07-01 22:44:45 +0900

    use template

--- app/main.py
+++ app/main.py
@@ -14,13 +14,13 @@ def hello_world():
 @app.get("/tasks", response_class=HTMLResponse)
 def show_all_tasks(request: Request):
     print("show todo list")
-    return templates.TemplateResponse("list-task-todo.html", {"request": request})
+    return templates.TemplateResponse("tasks-list.tmpl", {"request": request})
 
 
 @app.get("/tasks/new", response_class=HTMLResponse)
 def create_new_task(request: Request):
     print("create new task")
-    return templates.TemplateResponse("task-new.html", {"request": request})
+    return templates.TemplateResponse("task-new.tmpl", {"request": request})
 
 
 @app.get("/tasks/{task_id}", response_class=HTMLResponse)
